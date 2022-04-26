42ef3095d9b49946e06571857e3292eec93e367a
Sachin Chaubey <sachinchaubey205@gmail.com>
2022-04-26 23:30:54 +0530

    first commit

---
+++ inheriTe.java
@@ -0,0 +1,38 @@
+
+
+
+public class inheriTe{
+    public static void main(String[] args){
+        Car car = new Car();
+
+        Bicycle bicycle = new Bicycle();
+
+        // car.go();
+        // bicycle.speed();
+        System.err.println(car.tyre);
+        System.out.println(car.speed);
+        System.out.println(bicycle.paddle);
+    } 
+}
+
+class vehicle{
+    double speed;
+
+    void go(){
+        System.out.println("your vehicle is in speed");
+    }
+
+    void stop(){
+        System.out.println("your vehicle is stopped");
+    }
+}
+
+class Car extends vehicle{
+    int tyre = 4;
+    int door = 4;
+}
+
+class Bicycle extends vehicle{
+    int type = 3;
+    int paddle = 2;
+}
